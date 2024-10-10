fa9f42a58a8b3621ed22845ae497638a66d6bf25
Pian <muhamad.sufian.saharuddin@gmail.com>
2024-10-10 11:31:05 +0800

    Update postgres.sh

--- provision/installers/postgres.sh
+++ provision/installers/postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y postgresql-17 postgresql-server-dev-17 postgresql-17-postgis-3 postgresql-17-postgis-3-scripts
+sudo apt-get install -y postgresql-16 postgresql-server-dev-16 postgresql-16-postgis-3 postgresql-16-postgis-3-scripts
 
 sudo -u postgres psql -c "CREATE ROLE homestead LOGIN PASSWORD '$db_root_password' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/17/main/postgresql.conf
-sudo echo "host    all             all             8.0.2.2/32               md5" | tee -a /etc/postgresql/17/main/pg_hba.conf
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/16/main/postgresql.conf
+sudo echo "host    all             all             8.0.2.2/32               md5" | tee -a /etc/postgresql/16/main/pg_hba.conf
 
 sudo -u postgres /usr/bin/createdb --echo --owner=homestead homestead
 sudo service postgresql restart
